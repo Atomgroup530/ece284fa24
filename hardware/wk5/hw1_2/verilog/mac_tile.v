7cd378017366e47292ef3b4014d756da5be143b7
Atomgroup530 <xul007@ucsd.edu>
2024-11-18 23:27:07 -0800

    hw6 done

--- hardware/wk5/hw1_2/verilog/mac_tile.v
+++ hardware/wk5/hw1_2/verilog/mac_tile.v
@@ -14,17 +14,40 @@ input  [psum_bw-1:0] in_n;
 input  clk;
 input  reset;
 
-...
-...
+reg [1:0] inst_q;
+reg [bw-1:0] a_q;
+reg [bw-1:0] b_q;
+reg [psum_bw-1:0] c_q;
+reg load_ready_q;
+
+assign out_e = a_q;
+assign inst_e = inst_q;
 
 mac #(.bw(bw), .psum_bw(psum_bw)) mac_instance (
         .a(a_q), 
         .b(b_q),
         .c(c_q),
-	.out(mac_out)
+	.out(out_s)
 ); 
 
-...
-...
+always @(posedge clk) begin
+	if (reset) begin
+		inst_q <= 0;
+		load_ready_q <= 1;
+	end else begin
+		inst_q[1] <= inst_w[1];
+		if (inst_w[0] | inst_w[1]) begin
+			a_q <= in_w;
+			c_q <= in_n;
+		end
+		if (inst_w[0] & load_ready_q) begin
+			b_q <= in_w;
+			load_ready_q <= 1;
+		end
+		if (~load_ready_q) begin
+			inst_q[0] <= inst_w[0];
+		end
+	end
+end
 
 endmodule
